0c50edaa27d95c961aae5b4d937d678da885a331
opilane <opilane@HP-CZC2349HTM>
2023-04-19 12:25:59 +0300

    12

--- animerimine1.py
+++ animerimine1.py
@@ -2,6 +2,40 @@ import pygame
 import random
 import sys
 
+def K():
+    if posX==0 and posY==0:
+        sammX=1
+        sammY=0
+    if posX==X-pilt.get_rect().width and posY<=Y-pilt.get_rect().height:
+        sammX=0
+        sammY=1
+    if posX==X-pilt.get_rect().width and posY==Y-pilt.get_rect().height:
+        sammX=1
+        sammY=0
+        sammX=-sammX
+    if posX==0 and posY>=Y-pilt.get_rect().height:
+        sammX=0
+        sammY=1
+        sammY=-sammY
+    posX+=sammX
+    posY+=sammY
+
+def K180():
+    if posX==0 and posY==0:
+        sammX=0
+        sammY=1
+    if posX>=X-pilt.get_rect().width and posY==Y-pilt.get_rect().height:
+        sammX=-1
+        sammY=0
+    if posX==X-pilt.get_rect().width and posY>=Y-pilt.get_rect().height:
+        sammX=0
+        sammY=-1
+    if posX<=0 and posY==0:
+        sammX=1
+        sammY=0
+    posX+=sammX
+    posY+=sammY
+
 pygame.init()
 pind=pygame.display.set_mode((640,480))
 
@@ -27,6 +61,7 @@ posY=Y-pilt.get_rect().height
 posX1=X+pilt.get_rect().width
 posY1=Y+pilt.get_rect().height
 
+
 lõpp=False
 sammX=2
 sammY=0
@@ -36,107 +71,22 @@ while not lõpp:
     for i in pygame.event.get():
         if i.type==pygame.QUIT():
             sys.exit()
-    ekraan.blit(pilt,(posX,posY))
-    """
-    def K():
-    if posX==0 and posY==0:
-        sammX=1
-        sammY=0
-    if posX==X-pilt.get_rect().width and posY<=Y-pilt.get_rect().height:
-        sammX=0
-        sammY=1
-    if posX==X-pilt.get_rect().width and posY==Y-pilt.get_rect().height:
-        sammX=1
-        sammY=0
-        sammX=-sammX
-    if posX==0 and posY>=Y-pilt.get_rect().height:
-        sammX=0
-        sammY=1
-        sammY=-sammY
-    posX+=sammX
-    posY+=sammY
-    """
-
+    ekraan.blit(pilt,(posX,posY))    
     
-    
-    if posX==0 and posY==0:
-        sammX=0
-        sammY=1
-    if posX==X-pilt.get_rect().width and posY==Y-pilt.get_rect().height:
-        sammX=0
-        sammY=-1
-    if posX==X-pilt.get_rect().width and posY<=Y-pilt.get_rect().height:
-        sammX=0
-        sammY=-1
-    if posX<=0 and posY==0:
-        sammX=1
-        sammY=0
-    posX+=sammX
-    posY+=sammY
-    
-    """
-    if posX==0 and posY==0:
-        sammX=0
-        sammY=1
-    if posX>=X -pilt.get_rect().width and posY==Y-pilt.get_rect().height:
+    if posX>=X-pilt.get_rect().width and posY==Y-pilt.get_rect().height:
         sammX=-1
         sammY=0
     if posX==X-pilt.get_rect().width and posY>=Y-pilt.get_rect().height:
         sammX=0
         sammY=-1
-    if posX<= 0 and posY==0:
-        sammX=1
-        sammY=0
+    if posX<=0 and posY==0:
+        
+        sammY=-1
+
     posX+=sammX
-    posY+=sammY
-    """
-          
+    posY+=sammY            
     pygame.display.flip()
     ekraan.fill(roheline)
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
