e6873ed853bf1eb1fdf794d18681520e59c9d9d2
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-04-26 22:30:38 +0300

    12

---
+++ class1.py
@@ -0,0 +1,101 @@
+﻿import pygame
+import sys
+from random import*
+import random
+
+def Päripäev(): #По часовой
+    if posX == X - pilt.get_rect().width and posY == Y - pilt.get_rect().height:
+        sammX, sammY = -1, 0
+    elif posX == X - pilt.get_rect().width and posY == 0:
+        sammX, sammY = 0, 1
+    elif posX == 0 and posY == 0:
+        sammX, sammY = 1, 0
+    elif posX == 0 and posY == Y - pilt.get_rect().height:
+        sammX, sammY = 0, -1
+    posX += sammX
+    posY += sammY
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
+
+def Vastupäev(): #Против часовой
+    if posY <= 0 and posX >= ekraan.get_width() - pilt.get_width():
+        sammX = -2
+        sammY = 0
+    elif posY >= ekraan.get_height() - pilt.get_height() and posX >= ekraan.get_width() - pilt.get_width():
+        sammX = 0
+        sammY = -2
+    elif posY >= ekraan.get_height() - pilt.get_height() and posX <= 0:
+        sammX = 2
+        sammY = 0
+    elif posY <= 0 and posX <= 0:
+        sammX = 0
+        sammY = 2
+    posX += sammX
+    posY += sammY
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
+
+def R():#Оталкивается от кроёв
+    if posY <= 0:
+        sammX = random.choice([-2, -1, 1, 2])
+        sammY = random.choice([1, 2])
+    elif posY >= ekraan.get_height() - pilt.get_height():
+        sammX = random.choice([-2, -1, 1, 2])
+        sammY = random.choice([-1, -2])
+    elif posX >= ekraan.get_width() - pilt.get_width():
+        sammX = random.choice([-1, -2])
+        sammY = random.choice([-2, -1, 1, 2])
+    elif posX <= 0:
+        sammX = random.choice([1, 2])
+        sammY = random.choice([-2, -1, 1, 2])
+    posX += sammX
+    posY += sammY
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
+
+def y():#в вверх  в низ
+    if posY <= 0:
+        sammY = 2
+    elif posY >= ekraan.get_height() - pilt.get_height():
+        sammY = -2
+    posX += sammX
+    posY += sammY
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
+
+
+
+pygame.init()
+X, Y = 640, 480
+ekraan = pygame.display.set_mode([X, Y])
+pygame.display.set_caption("Maa objektid - Michael Odinokov")
+
+# Задаем цвет фона
+fon = (100, 255, 100)
+ekraan.fill(fon)
+
+# Загружаем картинку
+pilt = pygame.image.load("pika2.png")
+posX, posY = X - pilt.get_rect().width, Y - pilt.get_rect().height
+sammX, sammY = 0, 0
+
+lõpp=False
+
+kell=pygame.time.Clock()
+
+v=[Päripäev,Vastupäev,R,y]
+
+while  not lõpp:
+    kell.tick(120)
+    events=pygame.event.get()
+    for i in pygame.event.get():       
+        if i.type==pygame.QUIT:
+                lõpp=True
+        funktsioon=random.shuffel(v)           
+    ekraan.blit(pilt, (posX, posY))
+    pygame.display.flip()
+    pygame.time.delay(10)
