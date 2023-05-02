41e8d1ad3e1d20aff8910295fcc17731643be504
opilane <opilane@HP-CZC2349HTM>
2023-05-02 15:04:17 +0300

    12

--- Game.py
+++ Game.py
@@ -3,8 +3,6 @@ import random
 import sys
 from random import*
 
-
-
 pygame.init()
 X, Y =640, 480
 ekraan=pygame.display.set_mode([X, Y])
@@ -13,20 +11,39 @@ pygame.display.set_caption("Maa objektid - Michael Odinokov")
 # Задаем цвет фона
 fon = pygame.image.load("dzungli.png")
 
-
 # maugli
 mau=pygame.image.load("Maugli.png")
 posX, posY=X- mau.get_rect().width, Y-mau.get_rect().height
-samm=2
-
-
+if posY <= 0:
+    sammY = 2
+elif posY >= ekraan.get_height() - mau.get_height():
+    sammY = -2
+posY += sammY
+ekraan.blit(mau, (posX, posY))
+pygame.display.flip()
+pygame.time.delay(10)
 
 # õuna
-
 õuna=pygame.image.load("õuna.png")
-posX1, posY1=X- õuna.get_rect().width, Y-õuna.get_rect().height
+posX1, posY1=X-õuna.get_rect().width, Y-õuna.get_rect().height
 samm=5
-posX1, posY1=0,0
+posX1, posY1=10,10
+if posY <= 0:
+    sammY = 2
+elif posY >= ekraan.get_height() - õuna.get_height():
+    sammY = -2
+
+# Koldõuna
+kõuna=pygame.image.load("kõuna.png")
+posX2, posY2=X-õuna.get_rect().width, Y-kõuna.get_rect().height
+samm=5
+posX1, posY1=5,5
+if posY <= 0:
+    sammY = 2
+elif posY >= ekraan.get_height() - kõuna.get_height():
+    sammY = -2
+
+
 
 kell=pygame.time.Clock()
 gameover=False
@@ -40,14 +57,18 @@ while True:
     keys=pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         posX -= samm
-    if keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_RIGHT]:
         posX += samm
-    if keys[pygame.K_UP]:
+    elif keys[pygame.K_UP]:
         posY -= samm
-    if keys[pygame.K_DOWN]:
+    elif keys[pygame.K_DOWN]:
         posY += samm
-    ekraan.blit(fon, (0, 0))
+    
+   
     ekraan.blit(mau, (posX, posY))
-    ekraan.blit(õuna, (posX, posY))
+    ekraan.blit(õuna, (posX1, posY1))
+    ekraan.blit(kõuna, (posX2, posY2))
     pygame.display.flip()
+    pygame.time.delay(10)
+    ekraan.blit(fon, (0, 0))
 pygame.quit()
