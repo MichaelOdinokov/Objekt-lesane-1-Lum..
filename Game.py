d934414d5f1d9c747918153941ade4c4f875151b
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-05-03 00:25:58 +0300

    1223

--- Game.py
+++ Game.py
@@ -1,76 +1,89 @@
 ﻿import pygame
 import random
 import sys
-from random import*
+import time
+import datetime
 
 pygame.init()
-X, Y =640, 480
-ekraan=pygame.display.set_mode([X, Y])
+
+X, Y = 640, 480
+screen = pygame.display.set_mode([X, Y])
 pygame.display.set_caption("Maa objektid - Michael Odinokov")
 
-# Задаем цвет фона
-fon = pygame.image.load("dzungli.png")
+#Laadi alla pildid
+fon= pygame.image.load("dzungli.png")
+mau= pygame.image.load("Maugli.png")
+õuna= pygame.image.load("õuna.png")
+kõuna= pygame.image.load("kõuna.png")
 
-# maugli
-mau=pygame.image.load("Maugli.png")
-posX, posY=X- mau.get_rect().width, Y-mau.get_rect().height
-if posY <= 0:
-    sammY = 2
-elif posY >= ekraan.get_height() - mau.get_height():
-    sammY = -2
-posY += sammY
-ekraan.blit(mau, (posX, posY))
-pygame.display.flip()
-pygame.time.delay(10)
+#Kell
+kell = pygame.time.Clock()
 
-kell=pygame.time.Clock()
-# õuna
-õuna=pygame.image.load("õuna.png")
-posX1, posY1=X-õuna.get_rect().width, Y-õuna.get_rect().height
-samm=5
-posX1, posY1=10,10
-if posY <= 0:
-    sammY = 2
-elif posY >= ekraan.get_height() - õuna.get_height():
-    sammY = -2
+gameover = False
+start_kell = time.time()
 
+# Objektid
+mau_pos= [X - mau.get_rect().width, Y - mau.get_rect().height]
+mau_speed= 3
+#õuna
+õuna_pos= [random.randint(0, X - õuna.get_rect().width), 0]
+õuna_speed= 2
 
-# Koldõuna
-kell.tick(160)
-kõuna=pygame.image.load("kõuna.png")
-posX2, posY2=X-õuna.get_rect().width, Y-kõuna.get_rect().height
-samm=5
-posX1, posY1=5,5
-if posY <= 0:
-    sammY = 2
-elif posY >= ekraan.get_height() - kõuna.get_height():
-    sammY = -2
+#kõuna
+kõuna_pos= [random.randint(0, X - kõuna.get_rect().width), 0]
+kõuna_speed= 10
+Muutaja= 0
 
-# Muutaja
+nimi=input("Nimi:")
 
-
-kell=pygame.time.Clock()
-gameover=False
-
-while True:
+while not gameover:
     kell.tick(60)
-    events=pygame.event.get()
-    for event in events:
-        if event.type==pygame.QUIT:
-            gameover=True
-    keys=pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
+    if time.time() - start_kell >= 20:
+        with open("results.txt", "a") as f:
+            f.write(f"Player: {nimi}\n")
+            f.write(f"Date: {datetime.datetime.now()}\n")
+            f.write(f"Play time: {time.time() - start_kell} seconds\n")
+            f.write(f"Score: {Muutaja}\n")
+            f.write("\n")
+        print("Время вышло! Результаты сохранены в файл.")
+        gameover = True
+    # Mau_move
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        posX -= samm
+        mau_pos[0] -= mau_speed
     elif keys[pygame.K_RIGHT]:
-        posX += samm
+        mau_pos[0] += mau_speed
     elif keys[pygame.K_UP]:
-        posY -= samm
+        mau_pos[1] -= mau_speed
     elif keys[pygame.K_DOWN]:
-        posY += samm
-    ekraan.blit(mau, (posX, posY))
-    ekraan.blit(õuna, (posX1, posY1))
-    ekraan.blit(kõuna, (posX2, posY2))
+        mau_pos[1] += mau_speed
+
+    # kirrus, positsion
+    õuna_pos[1] += õuna_speed
+    if õuna_pos[1] > Y:
+        õuna_pos = [random.randint(0, X - õuna.get_rect().width), 0]
+
+    kõuna_pos[1] += kõuna_speed
+    if kõuna_pos[1] > Y:
+        kõuna_pos = [random.randint(0, X - kõuna.get_rect().width), 0]
+
+    if pygame.Rect(*mau_pos, *mau.get_size()).colliderect(pygame.Rect(*õuna_pos, *õuna.get_size())):
+        Muutaja+= 1
+        õuna_pos = [random.randint(0, X - õuna.get_rect().width), 0]
+
+    if pygame.Rect(*mau_pos, *mau.get_size()).colliderect(pygame.Rect(*kõuna_pos, *kõuna.get_size())):
+        Muutaja+= 5
+        kõuna_pos = [random.randint(0, X - kõuna.get_rect().width), 0]
+
+    screen.blit(fon, (0, 0))
+    screen.blit(mau, mau_pos)
+    screen.blit(õuna, õuna_pos)
+    screen.blit(kõuna, kõuna_pos)
+    font = pygame.font.SysFont(None, 30)
+    text = font.render("Score: " + str(Muutaja), True, (255, 255, 255))
+    screen.blit(text, (10, 10))
     pygame.display.flip()
-    pygame.time.delay(10)
-    ekraan.blit(fon, (0, 0))
-pygame.quit()
+pygame.quit() 
